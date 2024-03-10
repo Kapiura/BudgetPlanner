e1b9c356b873bac1a27a1a2ef4333774c210c92e
Kapiura <swinxten@gmail.com>
2024-03-10 21:54:45 +0100

    Init commit

---
+++ build-gigaBudget-Desktop_Qt_6_6_2_GCC_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/kapiura/dev/BudgetPlanner/build-gigaBudget-Desktop_Qt_6_6_2_GCC_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/kapiura/Qt/6.6.2/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/kapiura/Qt/6.6.2/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/lib64/ccache/gcc" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/lib64/ccache/g++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
