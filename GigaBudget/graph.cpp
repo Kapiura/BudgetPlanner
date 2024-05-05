5189bdc4ec4b06654db1ab41d229700bc7b4e2d9
Kapiura <swinxten@gmail.com>
2024-05-05 17:33:04 +0200

    Add new class - new class graph - creating graphs using datas from database - qmake file - add libraries belong to charts

--- GigaBudget/graph.cpp
+++ GigaBudget/graph.cpp
@@ -1,7 +1,11 @@
 #include "graph.h"
+#include <QChartView>
 #include <QDebug>
+#include <QPieSeries>
+#include <QtCharts>
 
-Graph::Graph(QObject *parent) : QObject{parent}
+Graph::Graph(QObject *parent)
+    : QObject{parent}
 {
     qDebug() << "Graph object has been created";
 }
@@ -9,4 +13,20 @@ Graph::Graph(QObject *parent) : QObject{parent}
 Graph::~Graph()
 {
     qDebug() << "Graph object has been delted";
+}
+
+bool Graph::updatePieSeries()
+{
+    qDebug() << "Creating pie sereis\n";
+    series->append("", 2);
+}
+
+bool Graph::addSeries()
+{
+    qDebug() << "Adding series from pie series\n";
+}
+
+bool Graph::setView()
+{
+    qDebug() << "Set view of series\n";
 }
--- GigaBudget/graph.h
+++ GigaBudget/graph.h
@@ -1,17 +1,29 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <QChartView>
+#include <QDebug>
 #include <QObject>
-// #include <QtCharts>
+#include <QPieSeries>
+#include <QtCharts>
 
 class Graph : public QObject
 {
+private:
+    QPieSeries *series = new QPieSeries();
+    QChart *chart = new QChart();
+    QChartView *chartView = new QChartView();
+
     Q_OBJECT
-  public:
+public:
     explicit Graph(QObject *parent = nullptr);
     ~Graph();
 
-  signals:
+    bool updatePieSeries();
+    bool addSeries();
+    bool setView();
+
+signals:
 };
 
 #endif // GRAPH_H
